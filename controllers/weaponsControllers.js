95daabb390881c957329d44d7b9a23c830cac8c4
foziaShareen <foziashareen1@gmail.com>
2025-05-06 18:59:45 +0500

    update and delete added

--- controllers/weaponsControllers.js
+++ controllers/weaponsControllers.js
@@ -23,4 +23,117 @@ const getWeapons=async(req,res)=>{
         });
     }
 }
-module.exports={getWeapons}
+
+const getShotgunById=async(req,res)=>{
+    try{
+        const id=req.params.id;
+        const shotgun = await db.query('SELECT * FROM advance_weapons.shotgun WHERE id=?',[id]);
+        if (!shotgun || shotgun.length === 0) { // Check if data is empty
+            return res.status(404).json({
+                status: false,
+                message: "No weapons data found"
+            });
+        }
+        return res.status(200).send({
+            status: true,
+            data: shotgun[0]
+        });
+
+    }
+    catch(error){
+        console.log(error,"error")
+        res.status(500).send({
+            status: false,
+            message: "Error in get shotgun by id API",
+            error: error
+        })
+
+
+
+    }
+}
+
+const createShotgunData=async(req,res)=>{
+    try{
+        const {model,gauge}= req.body
+        if(!model || !gauge){
+            return res.status(400).json({
+                status: false,
+                message: "Please provide all required fields"
+            });
+        }
+        
+        const shotgun = await db.query('INSERT INTO advance_weapons.shotgun(model,gauge) VALUES(?,?)',[model,gauge]);
+        return res.status(200).send({
+            status: true,
+            data: shotgun[0]
+        });
+    } catch(error){
+        console.log(error)
+        res.send({
+            status: false,
+            message: "Error in create shotgun data API",
+            error: error
+        });
+    }
+}
+const updateShotgunData=async(req,res)=>{
+    try{
+        const id = req.params.id;
+        if(!id){
+            return res.status(400).json({
+                status: false,
+                message: "Please provide all required fields"
+            });
+        }
+
+        const {model,gauge}= req.body
+        const data = await db.query('UPDATE advance_weapons.shotgun SET model=?,gauge=? WHERE id=?',[model,gauge,id]);
+        return res.status(200).send({
+            status: true,
+            data: data[0]
+        });
+
+    }
+    catch(error){
+        console.log(error)
+        if(!model || !gauge){
+            return res.status(400).json({
+                status: false,
+                message: "Please provide all required fields"
+            });
+        }
+
+
+
+    }
+}
+
+const deleteShotgunData=async(req,res)=>{
+    try{
+        const id = req.params.id;
+        if(!id){
+            return res.status(404).send({
+                status: false,
+                message: "please provide valid id"
+            });
+        }
+        const data = await db.query('DELETE FROM advance_weapons.shotgun WHERE id=?',[id]);
+        return res.status(200).send({
+                status: true,
+                data: data[0]
+            })
+
+    }
+    catch(error){
+        console.log(error)
+        
+            return res.status(400).json({
+                status: false,
+                message: "error in delete shotgun data API",
+                error: error
+            });
+        }
+    }
+
+module.exports={getWeapons,getShotgunById,createShotgunData,updateShotgunData,deleteShotgunData}
--- routes/weaponsRoutes.js
+++ routes/weaponsRoutes.js
@@ -1,8 +1,20 @@
 const { getWeapons } = require("../controllers/weaponsControllers.js");
+const { getShotgunById } = require("../controllers/weaponsControllers.js");
+const { createShotgunData } = require("../controllers/weaponsControllers.js");
+const { updateShotgunData } = require("../controllers/weaponsControllers.js");
+const { deleteShotgunData } = require("../controllers/weaponsControllers.js");
 const express = require('express');
 // routes object 
 const router = express.Router();
 
 //get all weapons
 router.get('/all',getWeapons);
+//get by id
+router.get('/id/:id',getShotgunById);
+//create shotgun data
+router.post('/create',createShotgunData);
+//update shotgun data
+router.put('/update/:id',updateShotgunData);
+//delete shotgun data
+router.delete('/delete/:id',deleteShotgunData);
 module.exports = router;
